df9806fce5affe6294a745e8c2ac69bc21c0f482
lopezsol <mariasollopez23@gmail.com>
2025-05-21 10:58:27 -0300

    se agrega rxresource

--- src/app/auth/pages/login-page/login-page.component.ts
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -1,23 +1,71 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
 
 import { AuthService } from '@auth/services/auth.service';
 import { FormUtils } from 'src/app/utils/form-utils';
-import { ErrorAlertComponent } from "../../../shared/components/error-alert/error-alert.component";
+import { ErrorAlertComponent } from '../../../shared/components/error-alert/error-alert.component';
+import { rxResource } from '@angular/core/rxjs-interop';
+import { User } from '@auth/interfaces/user.interface';
+import { UserLogin } from '@auth/interfaces/user-login.interface';
+import { catchError, of, tap } from 'rxjs';
+import { LoaderComponent } from '../../../shared/components/loader/loader.component';
 
 @Component({
   selector: 'app-login-page',
-  imports: [RouterLink, ReactiveFormsModule, ErrorAlertComponent],
+  imports: [
+    RouterLink,
+    ReactiveFormsModule,
+    ErrorAlertComponent,
+    LoaderComponent,
+  ],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.css',
 })
 export class LoginPageComponent {
   fb = inject(FormBuilder);
-  $hasError = signal(false);
   router = inject(Router);
+  $hasError = signal(false);
+  $user = signal<UserLogin | null>(null);
+  $formSubmitted = signal(false);
 
   authService = inject(AuthService);
+
+  $userLogin = computed<UserLogin | null>(() => {
+    if (!this.$formSubmitted()) return null;
+    console.log('entre al login');
+    const { email, password } = this.loginForm.value;
+    return { mail: email ?? '', password: password ?? '' };
+  });
+
+  $authResource = rxResource({
+    request: () => {
+      // Solo retorna el usuario si el formulario ha sido enviado
+      return { user: this.$userLogin() };
+    },
+    loader: ({ request }) => {
+      // Si no hay usuario o el formulario no se ha enviado, retorna un observable vacío
+      if (!request.user) return of(null);
+
+      return this.authService
+        .login(request.user.mail, request.user.password)
+        .pipe(
+          tap((isAuthenticated) => {
+            if (isAuthenticated) {
+              this.router.navigateByUrl('/characters');
+            }
+          }),
+          catchError((error) => {
+            // Manejar el error pero devolver un valor para que no se quede en estado de error
+            this.$hasError.set(true);
+            setTimeout(() => {
+              this.$hasError.set(false);
+            }, 2000);
+            return of(false); // Devuelve un valor para que no se quede en estado de error
+          })
+        );
+    },
+  });
 
   loginForm = this.fb.group({
     email: [
@@ -44,18 +92,20 @@ export class LoginPageComponent {
       return;
     }
 
-    const { email = '', password = '' } = this.loginForm.value;
+    // const { email = '', password = '' } = this.loginForm.value;
+    // this.$user.set({ mail: email!, password: password! });
+    this.$formSubmitted.set(true);
 
-    this.authService.login(email!, password!).subscribe((isAuthenticated) => {
-      if (isAuthenticated) {
-        this.router.navigateByUrl('/characters');
-        return;
-      }
+    // this.authService.login(email!, password!).subscribe((isAuthenticated) => {
+    //   if (isAuthenticated) {
+    //     this.router.navigateByUrl('/characters');
+    //     return;
+    //   }
 
-      this.$hasError.set(true);
-      setTimeout(() => {
-        this.$hasError.set(false);
-      }, 2000);
-    });
+    //   this.$hasError.set(true);
+    //   setTimeout(() => {
+    //     this.$hasError.set(false);
+    //   }, 2000);
+    // });
   }
 }
