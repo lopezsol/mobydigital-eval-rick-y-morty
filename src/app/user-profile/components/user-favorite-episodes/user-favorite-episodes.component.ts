4b29d8b9f9bb40c55a6bbae7a1b0b4119f1d4bb5
lopezsol <mariasollopez23@gmail.com>
2025-06-30 06:01:37 -0300

    funcionalidad eliminar favorito desde perfil agregada

--- src/app/user-profile/components/user-favorite-episodes/user-favorite-episodes.component.ts
+++ src/app/user-profile/components/user-favorite-episodes/user-favorite-episodes.component.ts
@@ -1,24 +1,20 @@
-import { Component, inject, input, signal } from '@angular/core';
+import { Component, inject, input, output, signal } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { ErrorComponent } from '@shared/components/error/error.component';
 import { LoaderComponent } from '@shared/components/loader/loader.component';
 import type { Episode } from '@episodes/interfaces/episode.interface';
 @Component({
   selector: 'user-favorite-episodes',
   // imports: [LoaderComponent, ErrorComponent],
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './user-favorite-episodes.component.html',
   styleUrl: './user-favorite-episodes.component.css',
 })
 export class UserFavoriteEpisodesComponent {
   $initialLimit = signal(15);
   $favoriteEpisodes = input.required<Episode[]>();
-  // $favoriteEpisodes = signal<Episode[]>([
-  //   {
-  //     id: 28,
-  //     name: 'The Ricklantis Mixup',
-  //     url: 'https://rickandmortyapi.com/api/episode/28',
-  //   },
-  // ]);
+
+  $favoriteEpisodeToDelete = output<number>();
 
   updateInitialLimit() {
     this.$initialLimit.update((current) => current + 5);
